51e10b8fef8da22be845578bc7e6638c8b345805
Timowest12 <timowester@gmail.com>
2021-11-30 10:38:18 +0100

    add setup

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react"
 
 class InputTodo extends Component {
-    state = {
-        title: ""
-      };
-      onChange = e => {
-          this.setState({
-            title: e.target.value
-          });
-        };
+  state = {
+    title: "",
+  }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: ""
+    });
+  };
   render() {
     return (
-      <form>
-        <input onChange={this.onChange} type="text" placeholder="Add todo..." value={this.state.title} />
+        <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
         <button>Submit</button>
       </form>
     )
